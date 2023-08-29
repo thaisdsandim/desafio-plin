805d934fcb7a53968eff4c9a0cd5816ed752eb13
thaisdsandim <contatothais.sandim@gmail.com>
2023-08-29 08:20:58 -0400

    [NEW] clima configs

--- src/app/clima/page.tsx
+++ src/app/clima/page.tsx
@@ -1,7 +1,35 @@
+"use client";
+
+import React, { useEffect } from 'react';
+import API_CONFIG from '@/utils/apiConfig';
+
 export default function Clima() {
+
+  const handleSearch = async () => {
+    try {
+      const response = await fetch(`${API_CONFIG.climaApi}`, {
+        headers: {
+          Accept: 'application/json'
+        }
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log('Clima:', data);
+      } else {
+        console.error('Erro ao buscar CEP:', response.status);
+      }
+    } catch (error) {
+      console.error('Erro ao buscar CEP:', error);
+    }
+  };
+
+  useEffect(() => {
+    handleSearch();
+  }, []);
+
   return (
-    <main>
-      Clima
-    </main>
-  )
+    <div>
+    </div>
+  );
 }
--- src/utils/apiConfig.ts
+++ src/utils/apiConfig.ts
@@ -1,6 +1,6 @@
 const API_CONFIG = {
   cepApi: 'http://cep.la',
-  climaApi: ''
+  climaApi: 'https://api.hgbrasil.com/weather?key=c0315cc2&user_ip=remote'
 };
 
 export default API_CONFIG;

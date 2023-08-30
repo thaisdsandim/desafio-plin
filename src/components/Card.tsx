3beec7e2b1644063cb2c58f7df74a61475df2374
thaisdsandim <contatothais.sandim@gmail.com>
2023-08-30 16:12:14 -0400

    [UPDATE] change measurement units and delete readme

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,4 @@
-import {useTranslations} from 'next-intl';
+import { useTranslations } from 'next-intl';
 
 interface WeatherData {
   weather: {
@@ -20,8 +20,15 @@ interface CardProps {
 
 type WeatherDescription = 'clear sky' | 'few clouds' | 'overcast clouds' | 'scattered clouds' | 'broken clouds' | 'shower rain' | 'light rain' | 'rain' | 'thunderstorm' | 'snow' | 'mist';
 
+function celsiusToFahrenheit(celsius: number): number {
+  return (celsius * 9/5) + 32;
+}
+
 export default function Card({ weatherData }: CardProps) {
   const t = useTranslations('Weather');
+  
+  const selectedLanguage = localStorage.getItem('selectedLanguage');
+
   const weatherBackgroundImages: Record<WeatherDescription, string> = {
     'clear sky': 'https://github.com/thaisdsandim/desafio-plin/blob/main/public/ceulimpo.jpg?raw=true',
     'few clouds': 'https://github.com/thaisdsandim/desafio-plin/blob/main/public/ceucomnuvens.jpg?raw=true',
@@ -39,6 +46,33 @@ export default function Card({ weatherData }: CardProps) {
   const weatherDescription = (weatherData?.weather[0].description.toLowerCase() as WeatherDescription) || '';
   const backgroundImageUrl = weatherBackgroundImages[weatherDescription] || '';
 
+  const temperatureCelsius = weatherData?.main.temp || 0;
+  const temperatureFahrenheit = celsiusToFahrenheit(temperatureCelsius);
+
+  let temperature: number;
+  let temperatureUnit: string;
+
+  if (selectedLanguage === 'pt' || selectedLanguage === 'es') {
+    temperature = temperatureCelsius;
+    temperatureUnit = '°C';
+  } else {
+    temperature = temperatureFahrenheit;
+    temperatureUnit = '°F';
+  }
+
+  let windSpeed: number;
+  let windUnit: string;
+
+  if (selectedLanguage === 'pt' || selectedLanguage === 'es') {
+    windSpeed = weatherData?.wind.speed || 0;
+    windUnit = 'km/h';
+  } else {
+    const mphConversionFactor = 0.621371;
+    windSpeed = (weatherData?.wind.speed || 0) * mphConversionFactor;
+    windUnit = 'mph';
+    windSpeed = parseFloat(windSpeed.toFixed(2));
+  }
+
   return (
     <div className="flex justify-center items-center h-screen">
       <div
@@ -53,10 +87,17 @@ export default function Card({ weatherData }: CardProps) {
         {weatherData && (
           <div className="text-center flex flex-col justify-center items-center mt-40 mb-40">
             <p className="text-black">{weatherData.name}</p>
-            <p className="text-6xl text-black font-semibold mb-10">{Math.round(weatherData.main.temp)}°C</p>
+            <p className="text-6xl text-black font-semibold mb-10">
+              {temperature}
+              {temperatureUnit}
+            </p>
             <div className="flex text-black mb-6">
               <p>{weatherData.main.humidity}% <p>{t('moisture')}</p></p>
-              <p className="ml-6">{weatherData.wind.speed} km/h<p>{t('wind')}</p></p>
+              <p className="ml-6">
+                {windSpeed}
+                {windUnit}
+                <p>{t('wind')}</p>
+              </p>
             </div>
           </div>
         )}
